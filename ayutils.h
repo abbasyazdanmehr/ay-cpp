0b3da04ab8f534c76c9dd9e92b73296a8b545534
abbasyazdanmehr <kjkj55047@gmail.com>
2023-05-03 18:48:47 +0430

    some renaming

--- ayutils.h
+++ ayutils.h
@@ -1,12 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
 string RandomString(int ch)
 {
-    char alpha[ch_MAX] = { 'a', 'b', 'c', 'd', 'e', 'f', 'g',
+    char alpha[26] = { 'a', 'b', 'c', 'd', 'e', 'f', 'g',
                           'h', 'i', 'j', 'k', 'l', 'm', 'n',
                           'o', 'p', 'q', 'r', 's', 't', 'u',
                           'v', 'w', 'x', 'y', 'z' };
     string result = "";
     for (int i = 0; i<ch; i++)
-        result = result + alpha[rand() % ch_MAX];
+        result = result + alpha[rand() % 26];
 
     return result;
 }
--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
 #include <bits/stdc++.h>
-#include "debug.h"
+#include "aydebug.h"
 
 using namespace std;
 
