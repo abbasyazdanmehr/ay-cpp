507ca1b2a84245fb81e6f3b8d63586b033346e88
abbasyazdanmehr <kjkj55047@gmail.com>
2023-05-03 18:45:53 +0430

    first commit

---
+++ debug.h
@@ -0,0 +1,201 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// ------------------------------------ print ------------------------------------
+
+// 1d array //
+template <typename T>
+void print(T a[], int n)
+{
+    cout << "{";
+    for (size_t i = 0; i < n; i++)
+    {
+        cout << a[i] << ", ";
+    }
+    cout << "}\n";
+}
+
+// 1d vector //
+template <typename T>
+void print(vector<T> v)
+{
+    cout << "{";
+    for (size_t i = 0; i < v.size(); i++)
+    {
+        cout << v[i] << ", ";
+    }
+    cout << "}\n";
+}
+
+// 1d queue //
+template <typename T>
+void print(queue<T> q)
+{
+    cout << "{";
+    while (!q.empty())
+    {
+        cout << q.front() << ", ";
+        q.pop();
+    }
+    cout << "}\n";
+}
+
+// 1d pair //
+template <typename T, typename U>
+void print(pair<T, U> p)
+{
+    cout << "(" << p.first << ", " << p.second << ")\n";
+}
+
+// vector of pairs //
+template <typename T, typename U>
+void print(vector<pair<T, U> /**/> vp)
+{
+    cout << "\n-- vector of pairs ---\n";
+    for (size_t i = 0; i < vp.size(); i++)
+    {
+        print(vp[i]);
+    }
+    cout << "----------------------\n\n";
+}
+
+// queue of pairs //
+template <typename T, typename U>
+void print(queue<pair<T, U> /**/> q)
+{
+    cout << "\n-- queue of pairs ---\n";
+    while (!q.empty())
+    {
+        print(q.front());
+        q.pop();
+    }
+    cout << "----------------------\n\n";
+}
+
+// 1d map //
+void print(map<int, int> m)
+{
+    map<int, int>::iterator it = m.begin();
+
+    cout << "{";
+    while (it != m.end())
+    {
+        cout << "(" << it->first << ": " << it->second << "),  ";
+        ++it;
+    }
+    cout << "}\n";
+}
+
+// //
+void print(map<char, int> m)
+{
+    map<char, int>::iterator it = m.begin();
+
+    cout << "{";
+    while (it != m.end())
+    {
+        cout << "(" << it->first << ": " << it->second << "),  ";
+        ++it;
+    }
+    cout << "}\n";
+}
+
+// //
+void print(map<string, int> m)
+{
+    map<string, int>::iterator it = m.begin();
+
+    cout << "{";
+    while (it != m.end())
+    {
+        cout << "(" << it->first << ": " << it->second << "),  ";
+        ++it;
+    }
+    cout << "}\n";
+}
+
+// //
+void print(map<string, string> m)
+{
+    map<string, string>::iterator it = m.begin();
+
+    cout << "{";
+    while (it != m.end())
+    {
+        cout << "(" << it->first << ": " << it->second << "),  ";
+        ++it;
+    }
+    cout << "}\n";
+}
+
+// //
+void print(map<char, char> m)
+{
+    map<char, char>::iterator it = m.begin();
+
+    cout << "{";
+    while (it != m.end())
+    {
+        cout << "(" << it->first << ": " << it->second << "),  ";
+        ++it;
+    }
+    cout << "}\n";
+}
+
+// ------------------------------------ random_initial ------------------------------------
+
+// 1d array //
+template <typename T>
+void random_initial(T a[], int n, int limit)
+{
+    for (size_t i = 0; i < n; i++)
+    {
+        a[i] = rand() % limit;
+    }
+}
+
+// 1d vector //
+template <typename T>
+void random_initial(vector<T> &v, int limit)
+{
+    for (size_t i = 0; i < v.size(); i++)
+    {
+        v[i] = rand() % limit;
+    }
+}
+
+// //
+template <typename T>
+void random_initial(vector<T> &v, int n, int limit)
+{
+    for (size_t i = 0; i < n; i++)
+    {
+        v.push_back(rand() % limit);
+    }
+}
+
+// 1d queue //
+template <typename T>
+void random_initial(queue<T> &q, int n, int limit)
+{
+    for (size_t i = 0; i < n; i++)
+    {
+        q.push(rand() % limit);
+    }
+}
+
+// ------------------------------------ misc ------------------------------------
+// //
+string random_string(int ch)
+{
+    char alpha[26] = {'a', 'b', 'c', 'd', 'e', 'f', 'g',
+                      'h', 'i', 'j', 'k', 'l', 'm', 'n',
+                      'o', 'p', 'q', 'r', 's', 't', 'u',
+                      'v', 'w', 'x', 'y', 'z'};
+    string result = "";
+    for (int i = 0; i < ch; i++)
+        result = result + alpha[rand() % 26];
+
+    return result;
+}
---
+++ main.cpp
@@ -0,0 +1,72 @@
+#include <bits/stdc++.h>
+#include "debug.h"
+
+using namespace std;
+
+int main()
+{
+    // ios_base::sync_with_stdio(false);
+    // cin.tie(NULL);
+    // cout.tie(NULL);
+
+    int a[20];
+    random_initial(a, 20, 40);
+    print(a, 20);
+
+    vector<int> v(20, 0);
+    random_initial(v, 40);
+    print(v);
+
+    queue<int> q;
+    random_initial(q, 6, 20);
+    print(q);
+
+    pair<int, char> p(1, 'k');
+    print(p);
+
+    pair<int, int> p1(1, 1), p2(2, 3), p3(3, 4), p4(4, 9);
+    vector<pair<int, int> /**/> vp;
+    vp.push_back(p1);
+    vp.push_back(p2);
+    vp.push_back(p3);
+    vp.push_back(p4);
+    print(vp);
+
+    queue<pair<int, int> /**/> qp;
+    qp.push(p1);
+    qp.push(p2);
+    qp.push(p3);
+    print(qp);
+
+    map<int, int> mii;
+    mii[0] = 1;
+    mii[2] = 1;
+    mii[4] = 3;
+    print(mii);
+
+    map<char, int> mci;
+    mci['c'] = 1;
+    mci['t'] = 1;
+    mci['k'] = 3;
+    print(mci);
+
+    map<string, int> msi;
+    msi["akbar"] = 1;
+    msi["taghi"] = 1;
+    msi["naghi"] = 3;
+    print(msi);
+
+    map<string, string> mss;
+    mss["akbar"] = "nasrin";
+    mss["taghi"] = "nooshin";
+    mss["naghi"] = "kobra";
+    print(mss);
+
+    map<char, char> mcc;
+    mcc['t'] = 'k';
+    mcc['b'] = 'j';
+    mcc['s'] = 'm';
+    print(mcc);
+
+    return 0;
+}
